8eeb4ca3aca5658cea8d25451ca261c5d07cf378
Craig Niles <niles.c@gmail.com>
2018-08-12 07:32:59 -0400

    Checking in changes that make it draw a wave graph using samples

--- main.cpp
+++ main.cpp
@@ -9,10 +9,13 @@ extern "C"
     #include <libavutil/avutil.h>
     #include <libavcodec/avcodec.h>
     #include <SDL/SDL.h>
+    #include <SDL/SDL_gfxPrimitives.h>
 }
 #endif
 
 #include <unistd.h>
+
+
 
 struct PacketQueue {
     AVPacketList *first_pkt, *last_pkt;
@@ -22,12 +25,74 @@ struct PacketQueue {
     SDL_cond *cond;
 };
 
+struct Pixel {
+   int value;
+   Pixel* next;
+};
+
+struct PixelQueue {
+    Pixel *first_pixel, *last_pixel;
+    int size;
+    SDL_mutex *mutex;
+    SDL_cond *cond;
+};
+
 PacketQueue audioq;
+PixelQueue drawq;
 
 void packet_queue_init(PacketQueue *q) {
     memset(q, 0, sizeof(PacketQueue));
     q->mutex = SDL_CreateMutex();
     q->cond = SDL_CreateCond();
+}
+
+void pixel_queue_init(PixelQueue* q) {
+    memset(q, 0, sizeof(PacketQueue));
+    q->mutex = SDL_CreateMutex();
+    q->cond = SDL_CreateCond();
+}
+
+int pixel_queue_get(PixelQueue * q, Pixel pixels[], int size) {
+    int ret = 0;
+    SDL_LockMutex(q->mutex);
+    int dropped = 0;
+    while (dropped < size && q->first_pixel != NULL) {
+        Pixel *pixel = &pixels[dropped];
+        if (q->first_pixel != NULL) {
+            Pixel *pixel0 = q->first_pixel;
+            *pixel = *pixel0;
+            q->first_pixel = pixel->next;
+            if (pixel->next == NULL) {
+                q->last_pixel = NULL;
+            }
+            q->size--;
+            dropped++;
+            delete pixel0;
+            ret = 1;
+        }
+    }
+    SDL_UnlockMutex(q->mutex);
+
+    return ret;
+}
+
+int pixel_queue_put(PixelQueue* q, int val) {
+    SDL_LockMutex(q->mutex);
+    
+    Pixel *pixel0 = new Pixel();
+
+    pixel0->next = NULL;
+    pixel0->value = val;
+
+    if (q->last_pixel == NULL) {
+        q->first_pixel = pixel0;
+    } else {
+        q->last_pixel->next = pixel0;
+    }
+    q->last_pixel = pixel0;
+    q->size++;
+    SDL_UnlockMutex(q->mutex);
+    return 0;
 }
 
 int packet_queue_put(PacketQueue *q, AVPacket *pkt) {
@@ -98,7 +163,6 @@ AVCodecContext* getDecoderFromStream(AVStream* stream) {
 
     AVCodecContext* codecCtx = stream->codec;
 
-
     codecCtx->request_sample_fmt = AV_SAMPLE_FMT_S16;
 
     AVCodec* codec = avcodec_find_decoder(codecCtx->codec_id);
@@ -126,33 +190,33 @@ int audio_decode_frame(AVCodecContext* codecCtx, uint8_t* audio_buf, int buf_siz
 
   for(;;) {
     while(audio_pkt_size > 0) {
-      int got_frame = 0;
-      len1 = avcodec_decode_audio4(codecCtx, &frame, &got_frame, &pkt);
-      if(len1 < 0) {
-	/* if error, skip frame */
-	audio_pkt_size = 0;
-	break;
-      }
-    audio_pkt_data += len1;
-    audio_pkt_size -= len1;
-    data_size = 0;
+        int got_frame = 0;
+        len1 = avcodec_decode_audio4(codecCtx, &frame, &got_frame, &pkt);
+        if (len1 < 0) {
+            /* if error, skip frame */
+    	    audio_pkt_size = 0;
+    	    break;
+        }
+        audio_pkt_data += len1;
+        audio_pkt_size -= len1;
+        data_size = 0;
     
-    if(got_frame) {
-	    data_size = av_samples_get_buffer_size(NULL, 
-                    codecCtx->channels,
-                    frame.nb_samples,
-                    codecCtx->sample_fmt,
-                    1);
+        if(got_frame) {
+	        data_size = av_samples_get_buffer_size(NULL, 
+                codecCtx->channels,
+                frame.nb_samples,
+                codecCtx->sample_fmt,
+                1);
 
-	assert(data_size <= buf_size);
-	memcpy(audio_buf, frame.data[0], data_size);
-      }
-      if(data_size <= 0) {
-	/* No data yet, get more frames */
-	continue;
-      }
-      /* We have data, return it and come back for more later */
-      return data_size;
+	        assert(data_size <= buf_size);
+	        memcpy(audio_buf, frame.data[0], data_size);
+        }
+        if(data_size <= 0) {
+	        /* No data yet, get more frames */
+	        continue;
+        }
+        /* We have data, return it and come back for more later */
+        return data_size;
     }
     if(pkt.data)
       av_free_packet(&pkt);
@@ -169,13 +233,26 @@ int audio_decode_frame(AVCodecContext* codecCtx, uint8_t* audio_buf, int buf_siz
   }
 }
 
+int16_t abs(int16_t a) {
+    if (a >= 0) return a;
+    return -a;
+}
+
+int16_t max(int16_t a, int16_t b) {
+    if (a > b) return a;
+    return b;
+}
+
 void audioCallback(void *userdata, Uint8 *stream, int len) {
     AVCodecContext *codecCtx = (AVCodecContext*)userdata;
     int len1, audio_size;
 
+    Uint8* streamStart = stream;
+    int desiredLen = len;
+
     static uint8_t audio_buf[(192000 * 3) / 2];
     static unsigned int audio_buf_size = 0;
-    static unsigned int audio_buf_index = 0;
+    static unsigned int audio_buf_index = 0; // the next index to write to
     while (len > 0) {
         if (audio_buf_index >= audio_buf_size) {
             audio_size = audio_decode_frame(codecCtx, audio_buf, sizeof(audio_buf));
@@ -195,7 +272,31 @@ void audioCallback(void *userdata, Uint8 *stream, int len) {
         stream += len1;
         audio_buf_index += len1;
     }
+
+  
+    if (len != 0) std::cerr << "We're not getting an aligned sample set for the fft" << std::endl;
+
+    int pixel_count = 512;
+
+    int pixels[pixel_count];
+
+    memset(pixels, 0, sizeof(pixels));
+
+    // get mono samples assuming 16-bit LRLR..LR order
+    for (int i = 0; i < desiredLen + len; i+=4) {
+        int16_t l_sample = *(int16_t *)&streamStart[i];
+        int16_t r_sample = *(int16_t *)&streamStart[i+2];
+
+        int pixel_idx = ((i / 4) / (512 / pixel_count) % pixel_count);
+
+        pixels[pixel_idx] = max(pixels[pixel_idx], abs((int(l_sample) + r_sample) >> 1));
+    }
+
+    for (int i = 0; i < pixel_count; ++i) {
+        pixel_queue_put(&drawq, pixels[i]);
+    }
 }
+
 
 void setupAudio(AVCodecContext* codecCtx) {
     SDL_AudioSpec desiredSpec;
@@ -214,16 +315,30 @@ void setupAudio(AVCodecContext* codecCtx) {
     }
 }
 
+
+
 int main(int, char**)
 {
+    SDL_Surface* screen = NULL;
+    SDL_Surface* gfx = NULL;
+
     SDL_Event event;
     av_register_all();
-    if(SDL_Init(SDL_INIT_AUDIO | SDL_INIT_TIMER)) {
+    if(SDL_Init(SDL_INIT_AUDIO | SDL_INIT_TIMER | SDL_INIT_VIDEO)) {
         abort();
     }
 
+    screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
+
+    gfx = SDL_CreateRGBSurface(SDL_SWSURFACE, 640, 480,
+        screen->format->BitsPerPixel,
+        screen->format->Rmask,
+        screen->format->Gmask,
+        screen->format->Bmask,
+        screen->format->Amask);
+
     AVFormatContext* s = NULL;
-    const char* url = "file:Peanuts-Theme.mp3";
+    const char* url = "file:stinkfist.flac";
     if (avformat_open_input(&s, url, NULL, NULL) != 0) {
         return -1;        
     }
@@ -235,7 +350,7 @@ int main(int, char**)
     av_dump_format(s, 0, url, 0);
     int streamIdx = -1;
     
-    for (int i = 0; i < s->nb_streams; ++i) {
+        for (int i = 0; i < s->nb_streams; ++i) {
         if (s->streams[i]->codec->codec_type == AVMEDIA_TYPE_AUDIO) {
             streamIdx = i;
             break;
@@ -249,12 +364,14 @@ int main(int, char**)
 
     std::cout << "Starting audio queue" << std::endl;
     packet_queue_init(&audioq);
+    pixel_queue_init(&drawq);
 
     std::cout << "Starting SDL audio" << std::endl;
     SDL_PauseAudio(0);
 
     std::cout << "Playing audio" << std::endl;
     AVPacket packet;
+
     while (av_read_frame(s, &packet)>=0) {
         if (packet.stream_index == streamIdx) {
             packet_queue_put(&audioq, &packet);
@@ -262,18 +379,49 @@ int main(int, char**)
             av_free_packet(&packet);
         }
         SDL_PollEvent(&event);
+    }
+
+    std::cout << "All packets added to queue" << std::endl;
+
+    int offset = 0;
+
+    while (!quit) {
+
+        bool draw = false;
+        Pixel pixels[640];
+        memset(pixels, 0, sizeof(pixels));
+
+        while(pixel_queue_get(&drawq, pixels, 640)) {
+            for (int i = 0; i< 640; ++i) {
+                if (pixels[i].value == 0) break;
+            float volume = (240.0 * ((float)pixels[i].value / INT16_MAX));
+
+            offset++;
+            if (offset >= 640) offset = 0;
+            
+            lineColor(gfx, offset, 0, offset, 480, 0x000000FF);
+            lineColor(gfx, offset, 240, offset, 240+volume, 0xFFFFFFFF);
+
+            }
+        }
+
+            SDL_Rect source_left = {0, 0, offset, 480};
+            SDL_Rect source_right = {offset, 0, 640-offset, 480};
+            SDL_Rect dest_left = {640-offset, 0, offset, 480};
+            SDL_Rect dest_right = {0, 0, 640-offset, 480};
+            SDL_BlitSurface(gfx, &source_left, screen, &dest_left);
+            SDL_BlitSurface(gfx, &source_right, screen, &dest_right);
+            SDL_Flip(screen);           
+
+
         switch (event.type) {
         case SDL_QUIT:
             quit = 1;
             SDL_Quit();
             exit(0);
-        break; 
+        break;
             default:
             break;
         }
     }
-    
-    std::cout << "All packets added to queue" << std::endl;
-
-    sleep(10000);
 }
