f368b3175e933be0208afc6b600b8fc0da24b7f8
Craig Niles <niles.c@gmail.com>
2019-10-11 23:27:39 +0000

    Change threshold value to be driven by command line argument and prevent video output. Make GPIO pins high/low to turn lights on and off instead.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <math.h>
 #include <algorithm>
-#include <SDL2_gfxPrimitives.h>
+#include <wiringPi.h>
 
 // Need this for CPlusPlus development with this C library
 #ifdef __cplusplus
@@ -23,6 +23,37 @@ const Uint16 SCREEN_H = 768;
 const Uint16 ZERO = 0;
 
 #define LIGHTS 8
+
+/*
+
+ +-----+-----+---------+------+---+-Pi ZeroW-+---+------+---------+-----+-----+
+ | BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |
+ +-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+
+ |     |     |    3.3v |      |   |  1 || 2  |   |      | 5v      |     |     |
+ |   2 |   8 |   SDA.1 |   IN | 1 |  3 || 4  |   |      | 5v      |     |     |
+ |   3 |   9 |   SCL.1 |   IN | 1 |  5 || 6  |   |      | 0v      |     |     |
+ |   4 |   7 | GPIO. 7 |   IN | 1 |  7 || 8  | 1 | ALT5 | TxD     | 15  | 14  |
+ |     |     |      0v |      |   |  9 || 10 | 1 | ALT5 | RxD     | 16  | 15  |
+ |  17 |   0 | GPIO. 0 |   IN | 0 | 11 || 12 | 0 | ALT0 | GPIO. 1 | 1   | 18  |
+ |  27 |   2 | GPIO. 2 |   IN | 0 | 13 || 14 |   |      | 0v      |     |     |
+ |  22 |   3 | GPIO. 3 |   IN | 0 | 15 || 16 | 0 | IN   | GPIO. 4 | 4   | 23  |
+ |     |     |    3.3v |      |   | 17 || 18 | 0 | IN   | GPIO. 5 | 5   | 24  |
+ |  10 |  12 |    MOSI |   IN | 0 | 19 || 20 |   |      | 0v      |     |     |
+ |   9 |  13 |    MISO |   IN | 0 | 21 || 22 | 0 | IN   | GPIO. 6 | 6   | 25  |
+ |  11 |  14 |    SCLK |   IN | 0 | 23 || 24 | 1 | IN   | CE0     | 10  | 8   |
+ |     |     |      0v |      |   | 25 || 26 | 1 | IN   | CE1     | 11  | 7   |
+ |   0 |  30 |   SDA.0 |   IN | 1 | 27 || 28 | 1 | IN   | SCL.0   | 31  | 1   |
+ |   5 |  21 | GPIO.21 |   IN | 1 | 29 || 30 |   |      | 0v      |     |     |
+ |   6 |  22 | GPIO.22 |   IN | 1 | 31 || 32 | 0 | IN   | GPIO.26 | 26  | 12  |
+ |  13 |  23 | GPIO.23 |   IN | 0 | 33 || 34 |   |      | 0v      |     |     |
+ |  19 |  24 | GPIO.24 | ALT0 | 0 | 35 || 36 | 0 | IN   | GPIO.27 | 27  | 16  |
+ |  26 |  25 | GPIO.25 |   IN | 0 | 37 || 38 | 0 | ALT0 | GPIO.28 | 28  | 20  |
+ |     |     |      0v |      |   | 39 || 40 | 0 | ALT0 | GPIO.29 | 29  | 21  |
+ +-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+
+ | BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |
+ +-----+-----+---------+------+---+-Pi ZeroW-+---+------+---------+-----+-----+
+ */
+const int light_pins[] = { 22,21,3,2,0,7,9,8, 5,6,26,27 };
 
 struct sampleset {
     int size;
@@ -72,15 +103,26 @@ void pixel_callback(Uint8 *stream, int desiredLen, int len) {
 
 void init_libs() {
     av_register_all();
-    if(SDL_Init(SDL_INIT_AUDIO | SDL_INIT_TIMER | SDL_INIT_VIDEO)) {
+    if(SDL_Init(SDL_INIT_AUDIO | SDL_INIT_TIMER)) {
         abort();
     }
 }
 
 int main(int argc, char** argv) {
+
+  std::cout << "Starting wiring pi" << std::endl;
+  if (wiringPiSetup() == -1) {
+    std::cout << "Error starting wiring pi" << std::endl;
+    return 1;
+  } else {
+    for(int i = 0; i < LIGHTS; ++i) pinMode(light_pins[i], OUTPUT);
+  }
+
     SDL_Event event;
 
     const char *url = argv[1];
+
+    const float threshold = atof(argv[2]);
 
     std::cout << "Playing file " << url << std::endl;
 
@@ -90,13 +132,13 @@ int main(int argc, char** argv) {
     std::cout << "Init libs" << std::endl;
     init_libs();
 
-    SDL_Window *screen = SDL_CreateWindow("LightComposer",
+    /*SDL_Window *screen = SDL_CreateWindow("LightComposer",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
         SCREEN_W, SCREEN_H,
-        SDL_WINDOW_SHOWN);
+        SDL_WINDOW_SHOWN);*/
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    //SDL_Renderer *renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
     std ::cout << "Creating surfaces" << std::endl;
 
@@ -123,18 +165,18 @@ int main(int argc, char** argv) {
         }
         
         if (fft_in_idx >= FFT_SAMPLE_SIZE) {
-            SDL_RenderClear(renderer);
+	  //            SDL_RenderClear(renderer);
             bool draw = false;
             fft_in_idx = 0;
             fftw_execute(p);
 
             int lights[LIGHTS];
 
-            memset(lights, 0, sizeof(lights));
+            memset(lights, 1, sizeof(lights));
             
-            SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-            SDL_Rect fill = {0, 0, SCREEN_W, SCREEN_H};
-            SDL_RenderFillRect(renderer, &fill);
+	    //            SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+	    //            SDL_Rect fill = {0, 0, SCREEN_W, SCREEN_H};
+	    //            SDL_RenderFillRect(renderer, &fill);
 
             double c0 = 16.35;            
 
@@ -164,11 +206,11 @@ int main(int argc, char** argv) {
             total /= (FFT_SAMPLE_SIZE / 2.0);
 
             // Draw octave
-            SDL_SetRenderDrawColor(renderer, 0xFF, 0, 0, SDL_ALPHA_OPAQUE);
+	    //            SDL_SetRenderDrawColor(renderer, 0xFF, 0, 0, SDL_ALPHA_OPAQUE);
             for (int i = 0; i < 10; ++i) {
                 int freq = pow(2.0, i) * c0;
                 int x = log2(freq) / 26.0 * SCREEN_W;
-                SDL_RenderDrawLine(renderer, x, 0, x, SCREEN_H);
+		//                SDL_RenderDrawLine(renderer, x, 0, x, SCREEN_H);
             }
 
             b = 0;
@@ -193,23 +235,29 @@ int main(int argc, char** argv) {
 
                 double x = log2(freqs[i]) / 26.0 * SCREEN_W;
 
-                if (r > 0.6) {
-                    lights[o] = 1;
+                if (r > threshold) {
+                    lights[o] = 0;
                 }
 
-                SDL_SetRenderDrawColor(renderer, 0, 0xFF * (1.0 - r), 0xFF*r, SDL_ALPHA_OPAQUE);
-                SDL_RenderDrawLine(renderer, x, SCREEN_H, x, SCREEN_H-vol);
+		//                SDL_SetRenderDrawColor(renderer, 0, 0xFF * (1.0 - r), 0xFF*r, SDL_ALPHA_OPAQUE);
+		//                SDL_RenderDrawLine(renderer, x, SCREEN_H, x, SCREEN_H-vol);
             }
 
-            for (int i = 0; i < LIGHTS; ++i) {
+	    for (int i = 0; i < LIGHTS; ++i) {
+	      std::cout << i << ":" << lights[i] << " ";
+	        digitalWrite(light_pins[i], lights[i]);
+	    }
+	    std::cout << std::endl;
+
+           /*for (int i = 0; i < LIGHTS; ++i) {
                 if (lights[i]) {
                     filledCircleColor(renderer, 50+50*i, 50, 20, 0xFFFFFFFF);
                 } else {
                     filledCircleColor(renderer, 50+50*i, 50, 20, 0xAAAAAAFF);
                 }
-            }
+		}*/
             
-            SDL_RenderPresent(renderer);
+	    //            SDL_RenderPresent(renderer);
         }
 
 
@@ -223,7 +271,7 @@ int main(int argc, char** argv) {
             default:
             break;
         }
-        SDL_UpdateWindowSurface(screen);
+	//        SDL_UpdateWindowSurface(screen);
     }
 
     fftw_destroy_plan(p);
